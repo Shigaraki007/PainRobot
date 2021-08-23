95ebb444f2b901a328904ef525bd80e9c7de56bc
Shigaraki007 <77790096+Shigaraki007@users.noreply.github.com>
2021-08-23 16:04:13 +0530

    Update dbcleanup.py

--- BoaHancockBOT/modules/dbcleanup.py
+++ BoaHancockBOT/modules/dbcleanup.py
@@ -1,9 +1,9 @@
 from time import sleep
 
-import SaitamaRobot.modules.sql.global_bans_sql as gban_sql
-import SaitamaRobot.modules.sql.users_sql as user_sql
-from SaitamaRobot import DEV_USERS, OWNER_ID, dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus
+import BoaHancockBOT.modules.sql.global_bans_sql as gban_sql
+import BoaHancockBOT.modules.sql.users_sql as user_sql
+from BoaHancockBOT import STRAWHATS, PIRATE_KING_ID, dispatcher
+from BoaHancockBOT.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (CallbackContext, CallbackQueryHandler, CommandHandler,
@@ -114,7 +114,7 @@ def callback_button(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     query_type = query.data
 
-    admin_list = [OWNER_ID] + DEV_USERS
+    admin_list = [PIRATE_KING_ID] + STRAWHATS
 
     bot.answer_callback_query(query.id)
 
