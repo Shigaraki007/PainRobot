b2f70f50f67ec29499c91c225a48e55cb62592e4
Shigaraki007 <77790096+Shigaraki007@users.noreply.github.com>
2021-08-23 18:10:59 +0530

    Update gettime.py

--- BoaHancockBOT/modules/gettime.py
+++ BoaHancockBOT/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from SaitamaRobot import TIME_API_KEY, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from BoaHancockBOT import TIME_API_KEY, dispatcher
+from BoaHancockBOT.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
