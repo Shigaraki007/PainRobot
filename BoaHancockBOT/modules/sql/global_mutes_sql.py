1e16b973e25a891c2b5cd76e444171343ec867da
Shigaraki007 <77790096+Shigaraki007@users.noreply.github.com>
2021-08-24 17:18:24 +0530

    Update global_mutes_sql.py

--- BoaHancockBOT/modules/sql/global_mutes_sql.py
+++ BoaHancockBOT/modules/sql/global_mutes_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
 
-from SaitamaRobot.modules.sql import BASE, SESSION
+from BoaHancockBOT.modules.sql import BASE, SESSION
 
 
 class GloballyMutedUsers(BASE):
