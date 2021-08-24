7b987f41f846b2d8fcee27313da9c5da0b1c39eb
Shigaraki007 <77790096+Shigaraki007@users.noreply.github.com>
2021-08-24 17:19:37 +0530

    Update locks_sql.py

--- BoaHancockBOT/modules/sql/locks_sql.py
+++ BoaHancockBOT/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from SaitamaRobot.modules.sql import SESSION, BASE
+from BoaHancockBOT.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
