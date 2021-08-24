3c125ba9d0b665079c97326d3e9b7d00d705475c
Shigaraki007 <77790096+Shigaraki007@users.noreply.github.com>
2021-08-24 18:19:49 +0530

    Update telegraph.py

--- BoaHancockBOT/modules/telegraph.py
+++ BoaHancockBOT/modules/telegraph.py
@@ -1,12 +1,12 @@
-from SaitamaRobot.events import register
-from SaitamaRobot import telethn as tbot
+from BoaHancockBOT.events import register
+from BoaHancockBOT import telethn as tbot
 TMP_DOWNLOAD_DIRECTORY = "tg-File/"
 from telethon import events
 import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-babe = "SaitamaRobot"
+babe = "BoaHancockBOT"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=babe)
 auth_url = r["auth_url"]
