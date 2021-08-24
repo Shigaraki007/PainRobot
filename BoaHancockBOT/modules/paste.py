09440da0a99b96cee50ae3f16c98b31734cd787c
Shigaraki007 <77790096+Shigaraki007@users.noreply.github.com>
2021-08-24 12:36:15 +0530

    Update paste.py

--- BoaHancockBOT/modules/paste.py
+++ BoaHancockBOT/modules/paste.py
@@ -1,6 +1,6 @@
 import requests
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from BoaHancockBOT import dispatcher
+from BoaHancockBOT.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
