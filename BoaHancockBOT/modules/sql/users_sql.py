a2dd2e52aeb3757952d744c8353494e14ff83bd3
Shigaraki007 <77790096+Shigaraki007@users.noreply.github.com>
2021-08-24 17:28:42 +0530

    Update users_sql.py

--- BoaHancockBOT/modules/sql/users_sql.py
+++ BoaHancockBOT/modules/sql/users_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.sql import BASE, SESSION
+from BoaHancockBOT import dispatcher
+from BoaHancockBOT.modules.sql import BASE, SESSION
 from sqlalchemy import (Column, ForeignKey, Integer, String, UnicodeText,
                         UniqueConstraint, func)
 
